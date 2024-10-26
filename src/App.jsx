939de243acea3c19688d25782ca4575d434fa8be
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2024-10-26 22:52:40 +0600

    knowledge cafe project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="mt-5 ml-8">
+        <h1 className="text-2xl font-bold">Knowlagde Cafe</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
